160a7e71bfab7941f0ffbb9096edb6ea2ab787e2
Manuel Rincón <manuelrincongamedev@gmail.com>
2022-04-12 00:47:56 +0200

    Añadido VFX PowerUp

---
+++ Assets/EventOnScoreAchieved.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnScoreAchieved : MonoBehaviour
+{
+    [SerializeField] UnityEvent OnScoreReached;
+    int[] objectives;
+    int objectiveValue;
+    ScoreManager scoreManager;
+
+    private void Start()
+    {
+        scoreManager = ScoreManager.instance;
+    }
+
+    public void ResetObjective()
+    {
+        objectiveValue = 0;
+    }
+
+    public void CheckObjective()
+    {
+        if (scoreManager.solidScore > objectives[objectiveValue])
+        {
+            objectiveValue++;
+            OnScoreReached.Invoke();
+        }
+    }
+
+}
