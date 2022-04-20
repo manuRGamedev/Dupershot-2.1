23a8fb96743b1196ee1e3a8790022007a063f50a
Manuel Rincón <manuelrincongamedev@gmail.com>
2022-04-20 22:18:43 +0200

    DuperBulletEffect

---
+++ Assets/Scripts/PitchScaleImpact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchScaleImpact : MonoBehaviour
+{
+    int impactValue;
+    [SerializeField] int maxImpactValue;
+    [SerializeField] float minPitch = 0.8f;
+    [SerializeField] float maxPitch = 1.6f;
+    [SerializeField] string soundToCall;
+
+    private void OnEnable()
+    {
+        impactValue = 0;
+    }
+
+    public void StackImpact()
+    {
+        float pitch = minPitch + maxPitch * Mathf.Min(impactValue, maxImpactValue);
+
+        impactValue++;
+        AudioManager.instance.PlayAtPitch(soundToCall, pitch);
+    }
+}
